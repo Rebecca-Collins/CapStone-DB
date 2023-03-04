fef8d2d5a88c76fb4dd0dd7ceebcb4271f1b7c26
Rebecca-Collins <rebeccacollins@shaw.ca>
2023-03-03 22:38:32 -0800

    added cont pool

--- index.js
+++ index.js
@@ -9,6 +9,46 @@ const commentsRoutes = require("./routes/commentsRoutes");
 const PORT = process.env.PORT || 8080;
 const fileupload = require('express-fileupload');
 
+
+
+// Create a MySQL connection pool using environment variables
+const pool = mysql.createPool({
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+});
+
+// Define a route to handle database queries
+app.get('/users', (req, res) => {
+  // Get a connection from the pool
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error connecting to MySQL database: ', err);
+      res.status(500).send('Internal server error');
+    } else {
+      // Execute a query on the database
+      connection.query('SELECT * FROM users', (error, results) => {
+        // Release the connection back to the pool
+        connection.release();
+        if (error) {
+          console.error('Error querying MySQL database: ', error);
+          res.status(500).send('Internal server error');
+        } else {
+          res.send(results);
+        }
+      });
+    }
+  });
+});
+
+
+
+
+
+
+
 // --MIDDLEWARE--
 app.use(cors());
 // app.use(cors({
