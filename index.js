e8d1e1fad646ac73ae6def942983d1019dbf600e
Rebecca-Collins <rebeccacollins@shaw.ca>
2023-03-03 23:06:07 -0800

    debugging

--- index.js
+++ index.js
@@ -11,37 +11,37 @@ const fileupload = require('express-fileupload');
 
 
 
-// Create a MySQL connection pool using environment variables
-const pool = mysql.createPool({
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-});
+// // Create a MySQL connection pool using environment variables
+// const pool = mysql.createPool({
+//   host: process.env.DATABASE_HOST,
+//   port: process.env.DATABASE_PORT,
+//   user: process.env.DATABASE_USER,
+//   password: process.env.DATABASE_PASSWORD,
+//   database: process.env.DATABASE_NAME,
+// });
 
-// Define a route to handle database queries
-app.get('/users', (req, res) => {
-  // Get a connection from the pool
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error('Error connecting to MySQL database: ', err);
-      res.status(500).send('Internal server error');
-    } else {
-      // Execute a query on the database
-      connection.query('SELECT * FROM users', (error, results) => {
-        // Release the connection back to the pool
-        connection.release();
-        if (error) {
-          console.error('Error querying MySQL database: ', error);
-          res.status(500).send('Internal server error');
-        } else {
-          res.send(results);
-        }
-      });
-    }
-  });
-});
+// // Define a route to handle database queries
+// app.get('/users', (req, res) => {
+//   // Get a connection from the pool
+//   pool.getConnection((err, connection) => {
+//     if (err) {
+//       console.error('Error connecting to MySQL database: ', err);
+//       res.status(500).send('Internal server error');
+//     } else {
+//       // Execute a query on the database
+//       connection.query('SELECT * FROM users', (error, results) => {
+//         // Release the connection back to the pool
+//         connection.release();
+//         if (error) {
+//           console.error('Error querying MySQL database: ', error);
+//           res.status(500).send('Internal server error');
+//         } else {
+//           res.send(results);
+//         }
+//       });
+//     }
+//   });
+// });
 
 
 
